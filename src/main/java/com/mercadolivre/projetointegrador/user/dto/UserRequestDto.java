ed52aea44ad4aefe441d27733398ecc243e7a750
renanmeli <renan.rsantana@mercadolivre.com>
2022-01-31 16:45:47 -0300

    validations ok

--- src/main/java/com/mercadolivre/projetointegrador/user/dto/UserRequestDto.java
+++ src/main/java/com/mercadolivre/projetointegrador/user/dto/UserRequestDto.java
@@ -3,6 +3,8 @@ package com.mercadolivre.projetointegrador.user.dto;
 import java.io.Serializable;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -18,16 +20,22 @@ public class UserRequestDto implements Serializable {
 
 	private static final long serialVersionUID = -992475568031422702L;
 
-	@CPF
+	@NotBlank(message = "cpf must not be empty")
+	@CPF//(message = "cpf invalid")
 	private String cpf;
 
+	@NotBlank(message = "name must not be empty")
 	private String name;
 
-	@Email
+	@NotBlank(message = "email must not be empty")
+	@Email(message = "email invalid")
 	private String email;
 
+	@NotBlank(message = "password must not be empty")
+	@Size(min = 5, message = "password must be at least 5 characters long")
 	private String password;
 
+	//@NotBlank(message = "userRole must not be empty")
 	private UserRole userRole;
 	
 	
