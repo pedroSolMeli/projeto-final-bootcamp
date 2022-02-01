70b9902990751ff4a1378c53ff1f28f653ea0afa
emanuellebesckow <emanuelle.besckow@mercadolivre.com>
2022-02-01 10:47:43 -0300

    inicializa requisito 2

--- src/main/java/com/mercadolivre/projetointegrador/purchaseorder/model/PurchaseOrder.java
+++ src/main/java/com/mercadolivre/projetointegrador/purchaseorder/model/PurchaseOrder.java
@@ -3,11 +3,19 @@ package com.mercadolivre.projetointegrador.purchaseorder.model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mercadolivre.projetointegrador.enums.OrderStatus;
+import com.mercadolivre.projetointegrador.product.model.Product;
+import com.mercadolivre.projetointegrador.user.model.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,6 +37,14 @@ public class PurchaseOrder implements Serializable{
     private Long id;
 
     private LocalDate orderDate;
+    
+    private User buyerId;
+    
+    private OrderStatus status;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "Product")
+    @JsonIgnoreProperties("PurchaseOrder")
+    private List<Product> products;
 
 //	userId USER
 //	orderStatus STRING/enum
--- src/main/java/com/mercadolivre/projetointegrador/user/repository/UserRepository.java
+++ src/main/java/com/mercadolivre/projetointegrador/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.mercadolivre.projetointegrador.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mercadolivre.projetointegrador.product.model.Product;
 import com.mercadolivre.projetointegrador.user.model.User;
 
 @Repository("UserRepository")
