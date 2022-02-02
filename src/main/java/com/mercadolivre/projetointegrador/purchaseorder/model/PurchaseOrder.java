8ddcb30c8270554ba765d6068b7f91e743a512bc
emanuellebesckow <emanuelle.besckow@mercadolivre.com>
2022-02-02 11:54:46 -0300

    model purchaseOrder relacionado

--- src/main/java/com/mercadolivre/projetointegrador/purchaseorder/model/PurchaseOrder.java
+++ src/main/java/com/mercadolivre/projetointegrador/purchaseorder/model/PurchaseOrder.java
@@ -10,8 +10,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.mercadolivre.projetointegrador.enums.OrderStatus;
 import com.mercadolivre.projetointegrador.purchaseProduct.model.PurchaseProduct;
 import com.mercadolivre.projetointegrador.user.model.User;
@@ -35,9 +38,12 @@ public class PurchaseOrder implements Serializable{
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+//	@Builder.Default
+	@JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate orderDate;
     
-    @OneToMany(cascade = CascadeType.ALL)
+	@ManyToOne
+    @JsonIgnoreProperties("orders")
     private User buyerId;
     
     private OrderStatus status;
--- src/main/java/com/mercadolivre/projetointegrador/user/model/User.java
+++ src/main/java/com/mercadolivre/projetointegrador/user/model/User.java
@@ -1,16 +1,23 @@
 package com.mercadolivre.projetointegrador.user.model;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mercadolivre.projetointegrador.batch.model.Batch;
 import com.mercadolivre.projetointegrador.enums.UserRole;
+import com.mercadolivre.projetointegrador.purchaseorder.model.PurchaseOrder;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,5 +49,10 @@ public class User implements Serializable {
 	private String password;
  
 	private UserRole userRole;
+	
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "buyerId")
+    @JsonIgnoreProperties("buyerId")
+    private List<PurchaseOrder> orders;
+
 
 }
