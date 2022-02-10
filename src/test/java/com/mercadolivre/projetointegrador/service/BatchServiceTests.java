dc65caaac8f981b57e2735bc92babcbc024970bd
emanuellebesckow <emanuelle.besckow@mercadolivre.com>
2022-02-10 13:24:59 -0300

    retorns exceção no getbyid

--- src/test/java/com/mercadolivre/projetointegrador/service/BatchServiceTests.java
+++ src/test/java/com/mercadolivre/projetointegrador/service/BatchServiceTests.java
@@ -3,6 +3,8 @@ package com.mercadolivre.projetointegrador.service;
 import java.util.Optional;
 
 import com.mercadolivre.projetointegrador.batch.service.BatchService;
+
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,7 +14,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mercadolivre.projetointegrador.batch.model.Batch;
 import com.mercadolivre.projetointegrador.batch.repository.BatchRepository;
@@ -40,6 +44,11 @@ public class BatchServiceTests {
 		Batch batchReturn = batchService.getById(2L);
 		//then
 		Assertions.assertEquals(2L, batchReturn.getId());
+	}
+	
+	@Test
+	public void shouldReturnBatchByIdAException() {
+		 Assertions.assertThrows(ResponseStatusException.class, () -> batchService.getById(1L));
 	}
 	
 
